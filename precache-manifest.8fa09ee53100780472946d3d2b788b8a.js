709bc47ef35f7a2f9db946db43af1c97119c40f2
AndreiR <andreir98@yahoo.com>
2020-09-21 20:00:34 +0300

    Updates

---
+++ precache-manifest.8fa09ee53100780472946d3d2b788b8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e2548acc77dcbfe5afe19e0b9e6fd1a",
+    "url": "/humansrolodex/index.html"
+  },
+  {
+    "revision": "7e0f8fe6ea5b368cd059",
+    "url": "/humansrolodex/static/css/main.3351bf0e.chunk.css"
+  },
+  {
+    "revision": "b7eb3d5953f55766fec6",
+    "url": "/humansrolodex/static/js/2.54e97ba0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/humansrolodex/static/js/2.54e97ba0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e0f8fe6ea5b368cd059",
+    "url": "/humansrolodex/static/js/main.e6d5bb25.chunk.js"
+  },
+  {
+    "revision": "49a24b7e9d2e705548be",
+    "url": "/humansrolodex/static/js/runtime-main.40741f9b.js"
+  }
+]);
